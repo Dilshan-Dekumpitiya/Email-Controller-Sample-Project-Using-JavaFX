70da092f942822750b65df64f58ef5b45cb3206b
Dilshan-Dekumpitiya <dilshandekumpitiya@gmail.com>
2023-07-17 23:58:41 +0530

    add dashboard

--- src/main/java/controller/LoginFormController.java
+++ src/main/java/controller/LoginFormController.java
@@ -1,4 +1,29 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginFormController {
+
+    @FXML
+    private AnchorPane loginContext;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) throws IOException {
+        Stage stage = (Stage) loginContext.getScene().getWindow();
+        stage.close();
+
+        Stage stage2 = new Stage();
+        stage2.setScene(new Scene(FXMLLoader.load(getClass().getResource("/dashboard-form.fxml"))));
+        stage2.setTitle("Dashboard");
+
+
+        stage2.show();
+    }
 }
